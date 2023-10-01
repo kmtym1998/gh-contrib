fbcc96975110be2c28302bfcf1122c1ed804b61c
kmtym1998 <kmtym.devloper@gmail.com>
2023-10-01 11:17:41 +0900

    upgrade go-pretty

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/cli/go-gh/v2/pkg/api"
 	"github.com/fatih/color"
-	"github.com/jedib0t/go-pretty/table"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/samber/lo"
 )
 
